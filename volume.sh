98ab93d315dca25ef0f20cc7bc9194f558a00a6e
simonafield <35753008+simonafield@users.noreply.github.com>
2026-07-20 14:06:15 -0700

    Added volume.sh

---
+++ volume.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+# Create .env file with UID and GID
+echo "UID=$(id -u)" > .env
+echo "GID=$(id -g)" >> .env
+
+docker compose \
+    --env-file .env \
+    -f docker-compose.yml \
+    -f compose-typst/docker-compose.yml \
+    -f compose-typst/docker-compose-util.yml \
+    run --rm \
+    alpine-volume
