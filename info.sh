00279d90e25e5301ff582b0a11eb7e9d6bb0a235
simonafield <35753008+simonafield@users.noreply.github.com>
2026-07-20 14:10:10 -0700

    Added fonts.sh and info.sh

---
+++ info.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+# Create .env file with UID and GID
+echo "UID=$(id -u)" > .env
+echo "GID=$(id -g)" >> .env
+
+docker compose \
+    --env-file .env \
+    -f compose-typst/docker-compose.yml \
+    -f compose-typst/docker-compose-util.yml \
+    run --rm \
+    typst-info
